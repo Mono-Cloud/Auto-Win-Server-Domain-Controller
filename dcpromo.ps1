c3ccf8ad44807460f7639f1153b3826d66bf84ca
Mono-Cloud <44344247+Mono-Cloud@users.noreply.github.com>
2019-10-16 15:05:19 +0100

    Update dcpromo.ps1

--- dcpromo.ps1
+++ dcpromo.ps1
@@ -1,2 +1,15 @@
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 
+Import-Module ADDSDeployment
+Install-ADDSDomainController `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$false `
+-CriticalReplicationOnly:$false `
+-DatabasePath “C:\Windows\NTDS” `
+-DomainName “dcpt.domain” `
+-InstallDns:$true `
+-LogPath “C:\Windows\NTDS” `
+-NoRebootOnCompletion:$false `
+-SiteName “Default-First-Site-Name” `
+-SysvolPath “C:\Windows\SYSVOL” `
+-Force:$true
